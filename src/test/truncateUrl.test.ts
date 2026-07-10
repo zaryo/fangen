a9645b9b23465a47dd66e937b96efffc4d940e30
Luiz Miguel <luiz13miguel24@gmail.com>
2026-07-10 14:52:40 -0300

    add more tests to cover truncateUrl() (#41)

--- src/test/truncateUrl.test.ts
+++ src/test/truncateUrl.test.ts
@@ -13,3 +13,16 @@ test("testTruncateUrlDoesNotTrimUrlEnd", () => {
 
   expect(truncateUrl(url, 30)).not.toBe(truncatedEndUrl);
 });
+
+test("testTruncateUrlDoesNotTrimSmallUrl", () => {
+  const url = "https://example.com/streaming";
+
+  expect(truncateUrl(url, 30)).toBe(url);
+});
+
+test("testTruncateUrlDoesNotTrimUrlStart", () => {
+  const url = "https://example.com/streaming/video.m3u8";
+  const truncatedEndUrl = "...https://example.com/streaming/video.m";
+
+  expect(truncateUrl(url, 30)).not.toBe(truncatedEndUrl);
+});
